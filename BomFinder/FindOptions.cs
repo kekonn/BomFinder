a1c5815b3709fb37b3a44c5cdc1f7d698bacabdc
Jonas Van der Aa <ketsukonn@gmail.com>
2020-05-08 15:38:00 +0200

    First commit

---
+++ BomFinder/FindOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CommandLine;
+
+namespace BomFinder
+{
+    [Verb("find", isDefault: true, HelpText = "Find files and print them")]
+    public class FindOptions
+    {
+        [Option('s',"search-pattern", Required = true, HelpText = "The pattern that defines what files to look for, i.e. \"*.cs\"")]
+        public string SearchPattern { get; set; }
+
+        [Option('d',"directory", Required = false, Default = "", HelpText = "The directory to search. Defaults to current directory if unspecified.")]
+        public string Directory { get; set; }
+
+        [Option('r',"remove-bom", Required = false, Default = false, HelpText = "When true, removes the BOM from the found files.")]
+        public bool RemoveBOM { get; set; }
+    }
+}
---
+++ BomFinder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Unicode;
+using CommandLine;
+
+namespace BomFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<FindOptions>(args)
+                .WithParsed(RunFind);
+        }
+
+        private static void RunFind(FindOptions options)
+        {
+            var directory = string.IsNullOrWhiteSpace(options.Directory)?Environment.CurrentDirectory:options.Directory;
+
+            var files = FindFiles(directory, options.SearchPattern);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            if (options.RemoveBOM)
+            {
+                Console.WriteLine("Removing BOM");
+
+                files.AsParallel().ForAll(RemoveBOM);
+
+                Console.WriteLine("BOM revomed on all files");
+            }
+        }
+
+        private static string[] FindFiles(string directory, string searchPattern)
+        {
+            return Directory.GetFiles(directory, searchPattern, SearchOption.AllDirectories).AsParallel().Where(f => HasBom(f)).ToArray();
+        }
+
+        private static bool HasBom(string fileName)
+        {
+            using var fs = File.OpenRead(fileName);
+
+            var bom = new byte[4];
+            var bytesRead = fs.Read(bom, 0, 4);
+
+            return bytesRead == 4 &&
+                (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf);
+
+        }
+
+        private static void RemoveBOM(string fileName)
+        {
+            var contents = File.ReadAllText(fileName);
+            File.WriteAllText(fileName, contents, new UTF8Encoding(false));
+        }
+    }
+}
